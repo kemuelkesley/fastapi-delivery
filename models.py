be542e816e8b4f1af4f8c00eca0afe451ea5217c
kemuelkesley <kemuelkesleytkd@outlook.com>
2025-10-30 11:25:10 -0300

    construção do modelo de banco de dados.

---
+++ models.py
@@ -0,0 +1,70 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils import ChoiceType
+
+
+
+# Cria a conexão com o banco de dados 
+db = create_engine("sqlite:///banco.db")
+
+# Cria a base do banco de dados
+Base = declarative_base()
+
+
+# Criar as classe/tabelas do banco de dados
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False)
+    senha = Column("Senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+        
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    # STATUS_PEDIDO = (
+    #     ("PENDENTE", "Pendente"),
+    #     ("CANCELADO", "Cancelado"),
+    #     ("FINALIZADO", "Finalizado"),
+    # )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    #tatus = Column("status", ChoiceType(STATUS_PEDIDO)) # pendente, cancelado, finalizado
+    status = Column("status", String)
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+    #itens = 
+
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.preco = preco
+        self.status = status
+
+
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("sabor", String)
+    tamanho = Column("tamanho", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+
+    def __init__ (self, quantidade, sabor, tamanho, preco_unitario, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
